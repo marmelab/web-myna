a09049f656d2fde66214cac825fa520324c05eb2
Alexis Janvier <contact@alexisjanvier.net>
2019-10-18 13:25:19 +0200

    Make proxy from configuration

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import express from "express";
 import signale from "signale";
+import proxy from "http-proxy-middleware";
 
 import config from "./config.js";
 
 if (!config.apis.length) {
   signale.error("There is no API to mock !");
-}
-
-for (const api of config.apis) {
-  signale.info(
-    `We will mock ${api.name} on url ${api.url} with token ${api.token}`
-  );
+  throw new Error("Thers is no API to mock");
 }
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Web Myna"));
+app.get("/", (req, res) => res.json({ message: "Hello Web Myna" }));
+
+for (const api of config.apis) {
+  const proxyOptions = {
+    changeOrigin: true,
+    proxyTimeout: 5000,
+    timeout: 5000,
+    target: api.url,
+    ignorePath: false,
+    pathRewrite: path => path.replace(`/${api.name}`, ""),
+    onProxyReq: proxyReq =>
+      proxyReq.setHeader("authorization", `Bearer ${api.token}`)
+  };
+  app.use(`/${api.name}`, proxy(proxyOptions));
+}
 
 app.listen(config.port, () => {
   signale.info(
