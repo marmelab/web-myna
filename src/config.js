3c22c5a7a2571747cf5863146eecc4a05e20aa4f
Alexis Janvier <contact@alexisjanvier.net>
2019-10-18 11:48:06 +0200

    Get Api configuration from convict
    with small hack to get token from environment variables

--- src/config.js
+++ src/config.js
@@ -1,6 +1,55 @@
 import convict from "convict";
+import path from "path";
+
+const processEnv = process.env;
+
+convict.addFormat({
+  name: "apis",
+  validate: (apis, schema) => {
+    if (!Array.isArray(apis)) {
+      throw new Error("must be of type Array");
+    }
+
+    apis.map((api, index) => {
+      const tokenName = api.name ? `${api.name.toUpperCase()}_TOKEN` : null;
+      if (tokenName) {
+        const apiToken = processEnv[tokenName];
+        apis[index].token = processEnv[tokenName];
+      }
+    });
+
+    for (const api of apis) {
+      convict(schema.children)
+        .load(api)
+        .validate();
+    }
+  }
+});
 
 const config = convict({
+  apis: {
+    doc: "A collection of APIs to mock.",
+    format: "apis",
+    default: [],
+    children: {
+      name: {
+        doc:
+          "The API name, used as id. So it have to be a slug format, but steel readable",
+        format: String,
+        default: null
+      },
+      url: {
+        doc: "The API main endpoint URL",
+        format: "url",
+        default: null
+      },
+      token: {
+        doc: "Authorization token add in headers",
+        format: String,
+        default: null
+      }
+    }
+  },
   env: {
     doc: "The application environment.",
     format: ["production", "development", "test"],
@@ -16,6 +65,7 @@ const config = convict({
   }
 });
 
+config.loadFile(path.resolve(path.dirname(""), "apis.json"));
 config.validate({ allowed: "strict" });
 
 export default config.getProperties();
--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import signale from "signale";
 
 import config from "./config.js";
 
+if (!config.apis.length) {
+  signale.error("There is no API to mock !");
+}
+
+for (const api of config.apis) {
+  signale.info(
+    `We will mock ${api.name} on url ${api.url} with token ${api.token}`
+  );
+}
+
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello Web Myna"));
