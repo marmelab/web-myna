b17ecb505a4f4057de111dee731ed45357a947d5
Alexis Janvier <contact@alexisjanvier.net>
2019-11-10 12:39:16 +0100

    Improve missing tokens display in express env

--- src/app.js
+++ src/app.js
@@ -2,25 +2,33 @@ import express from 'express';
 import proxy from 'http-proxy-middleware';
 
 import config from './config.js';
+import { MODE_RECORDER } from './modes.js';
 import { myna } from './myna.js';
 
 export const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'Hello Web Myna' }));
+app.get('/', (__, res) => res.json({ message: 'Hello Web Myna' }));
 
 for (const api of config.apis) {
-    const proxyOptions = {
-        changeOrigin: true,
-        proxyTimeout: 5000,
-        timeout: 5000,
-        target: api.url,
-        ignorePath: false,
-        pathRewrite: path => path.replace(`/${api.name}`, ''),
-        onProxyReq: proxyReq => {
-            if (api.requiresAuthentication) {
-                proxyReq.setHeader(`${api.tokenKey || 'authorization'}`, `${api.tokenPrefix || 'Bearer'} ${api.token}`);
-            }
-        },
-    };
-    app.use(`/${api.name}`, myna(api), proxy(proxyOptions));
+    if (config.mode === MODE_RECORDER) {
+        const proxyOptions = {
+            changeOrigin: true,
+            proxyTimeout: 5000,
+            timeout: 5000,
+            target: api.url,
+            ignorePath: false,
+            pathRewrite: path => path.replace(`/${api.name}`, ''),
+            onProxyReq: proxyReq => {
+                if (api.requiresAuthentication) {
+                    proxyReq.setHeader(
+                        `${api.tokenKey || 'authorization'}`,
+                        `${api.tokenPrefix || 'Bearer'} ${api.token}`,
+                    );
+                }
+            },
+        };
+        app.use(`/${api.name}`, myna(api), proxy(proxyOptions));
+    } else {
+        app.use(`/${api.name}`, myna(api));
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import signale from 'signale';
+import boxen from 'boxen';
 
 import * as appJs from './app.js';
-import config, { getMissingEnvironmentTokens } from './config.js';
+import config, { getApiTokenName, getMissingEnvironmentTokens } from './config.js';
 
 const missingTokens = getMissingEnvironmentTokens();
 
 if (missingTokens.length) {
-    signale.error('IL MANQUE DES TOKENS', missingTokens);
+    const missingApisWithAuthentication = config.apis
+        .filter(api => missingTokens.includes(getApiTokenName(api)))
+        .map(api => api.name);
+    const isPlural = missingApisWithAuthentication.length > 1;
+    const message = `
+You have declared ${missingApisWithAuthentication.length} api${
+        isPlural ? 's' : ''
+    } as requiring an authentication token: ${missingApisWithAuthentication.join(', ')}.
+
+You must therefore declare the following token${isPlural ? 's' : ''} as an environment variable: ${missingTokens
+        .map(
+            token => `
+ => ${token}`,
+        )
+        .join('')}`;
+    signale.log(boxen(message, { padding: 1, margin: 1, borderColor: 'red', align: 'center' }));
 } else {
     appJs.app.listen(config.proxyPort, () => {
         signale.info(`Web Myna is starded on port ${config.proxyPort} in environment ${config.env}`);
